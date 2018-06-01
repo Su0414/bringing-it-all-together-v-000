a210f05462ac12bf854f77696060d30d2f36c068
Suchitra Naik <susanaik@gmail.com>
2018-06-01 14:10:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Dog
   attr_accessor :name, :breed, :id
 
@@ -38,8 +40,11 @@ class Dog
                   INSERT INTO dogs (name, breed)
                   VALUES (?,?)
               SQL
-              DB[:conn].execute(sql, self.name, self.breed)
+              resultset = DB[:conn].execute(sql, self.name, self.breed)
+              binding.pry
+
               @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+              binding.pry
     end
 
   end
